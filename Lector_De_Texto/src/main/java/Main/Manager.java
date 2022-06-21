ac68f5e76caa749b9a52b2fe87156c2da5ae33d0
MatRN <51181381+MatyRN@users.noreply.github.com>
2022-06-21 02:23:37 -0300

    Add files via upload

---
+++ Lector_De_Texto/src/main/java/Main/Manager.java
@@ -0,0 +1,18 @@
+package Main;
+import java.io.*;
+/**
+ *
+ * @author MatRN
+ */
+public class Manager{
+   
+    public static void LeerFichero(){
+
+    }
+    public void EscribirArchivo(){
+        
+    }
+    public void GuardarArchivos(){
+        System.out.println("Funciona");
+    }
+}
